9abfcc6fd2060362ac9acad1bc755bbbb8201ede
Paul Ambrose <pambrose@mac.com>
2017-05-26 16:50:41 -0700

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,60 @@
-# snap-proxy-python
-Snap Proxy
+# Snap Unicorn Hat Server
+
+The Snap Unicorn HAT Server allows [Snap](http://snap.berkeley.edu) programs to manipulate
+a [Unicorn HAT](https://shop.pimoroni.com/products/unicorn-hat).
+
+The snap blocks roughly match the [Unicorn HAT API](http://docs.pimoroni.com/unicornhat/).
+ 
+![Snap Blocks](https://github.com/athenian-robotics/snap-unicorn-hat-gateway/raw/master/docs/snap-blocks.jpg "Snap Blocks")
+
+
+## Server Setup
+
+The server executes with python on a Raspberry Pi. 
+
+Install python and the Unicorn HAT software with: (details are [here](https://github.com/pimoroni/unicorn-hat))
+```bash
+$ curl -sS https://get.pimoroni.com/unicornhat | bash
+```
+
+Install the required python packages with:
+```bash
+$ pip install flask
+```
+
+Install git with:
+```bash
+$ sudo apt-get install git
+```
+Install the server with:
+```bash
+$ cd ~pi
+$ mkdir git
+$ cd git
+$ git clone https://github.com/athenian-robotics/snap-unicorn-hat-gateway.git
+```
+
+## Server Execution
+
+Execute the server with:
+```bash
+$ cd ~pi/git/snap-unicorn-hat-gateway
+$ sudo ./server.py
+```
+
+## Snap Blocks Installation
+
+To install the Unicorn HAT blocks:
+ 
+1) Download the 
+[block definitions](https://raw.githubusercontent.com/athenian-robotics/snap-unicorn-hat-gateway/master/snap/UnicornHatBlocks.xml) 
+to the computer running your browser. You can copy and paste it into an editor or use `wget`:
+```bash
+$ wget https://raw.githubusercontent.com/athenian-robotics/snap-unicorn-hat-gateway/master/snap/UnicornHatBlocks.xml
+```
+
+2) Start [snap](http://snap.berkeley.edu/snapsource/snap.html) in your browser
+
+3) Click on the page icon in the upper left hand corner of the snap window (to the left of the cloud icon)
+and then click on `Import...` and choose the *UnicornHatBlocks.xml* file created in step #1.
+
---
+++ server.py
@@ -0,0 +1,113 @@
+#!/usr/bin/env python
+
+import argparse
+import logging
+import sys
+
+import unicornhat as unicorn
+from flask import Flask
+from flask import make_response
+from flask import request
+
+LOG_LEVEL = "loglevel"
+
+logger = logging.getLogger(__name__)
+
+flask = Flask(__name__)
+
+
+@flask.route("/clear")
+def clear():
+    unicorn.clear()
+    return response()
+
+
+@flask.route("/show")
+def show():
+    unicorn.show()
+    return response()
+
+
+@flask.route("/x")
+def x():
+    return response(unicorn.get_shape()[0])
+
+
+@flask.route("/y")
+def y():
+    return response(unicorn.get_shape()[1])
+
+
+@flask.route("/set_all")
+def set_all():
+    r = request.args.get("r")
+    g = request.args.get("g")
+    b = request.args.get("b")
+    unicorn.set_all(r=int(r), g=int(g), b=int(b))
+    return response()
+
+
+@flask.route("/set_pixel")
+def set_pixel():
+    x = request.args.get("x")
+    y = request.args.get("y")
+    r = request.args.get("r")
+    g = request.args.get("g")
+    b = request.args.get("b")
+    unicorn.set_pixel(x=int(x), y=int(y), r=int(r), g=int(g), b=int(b))
+    return response()
+
+
+@flask.route("/get_pixel")
+def get_pixel():
+    x = request.args.get("x")
+    y = request.args.get("y")
+    val = str(unicorn.get_pixel(x=int(x), y=int(y)))[1:-1]
+    return response(val)
+
+
+@flask.route("/rotation/<int:rotation>")
+def rotation(rotation):
+    unicorn.rotation(rotation)
+    return response()
+
+
+@flask.route("/brightness/<float:brightness>")
+def brigtness(brightness):
+    unicorn.brightness(brightness)
+    return response("")
+
+
+@flask.route("/get_brightness")
+def get_brightness():
+    return response(unicorn.get_brightness())
+
+
+def response(val=""):
+    resp = make_response(str(val), 200)
+    resp.headers["Content-Type"] = "text/plain"
+    resp.headers["Access-Control-Allow-Origin"] = "http://snap.berkeley.edu"
+    resp.headers["Access-Control-Allow-Methods"] = "GET, POST"
+    return resp
+
+
+def setup_logging(filename=None,
+                  filemode="a",
+                  stream=sys.stderr,
+                  level=logging.INFO,
+                  format="%(asctime)s %(name)-10s %(funcName)-10s():%(lineno)i: %(levelname)-6s %(message)s"):
+    if filename:
+        logging.basicConfig(filename=filename, filemode=filemode, level=level, format=format)
+    else:
+        logging.basicConfig(stream=stream, level=level, format=format)
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-v", "--verbose", dest=LOG_LEVEL, default=logging.INFO, action="store_const",
+                        const=logging.DEBUG, help="Enable debugging info")
+    args = vars(parser.parse_args())
+
+    setup_logging(level=args[LOG_LEVEL])
+
+    flask.run(host="0.0.0.0", port=9000)
